86e6a23bcc814346d6552a6072d585d949fc290f
c0a09x5 <carlos.aparicio@walmart.com>
2023-09-18 22:17:36 -0300

    Eliminando archivos no deseados y agregando constraints a Usuario.java

--- msvc-usuarios/src/main/java/com/caparicio/springcloud/msvc/usuarios/models/entity/Usuario.java
+++ msvc-usuarios/src/main/java/com/caparicio/springcloud/msvc/usuarios/models/entity/Usuario.java
@@ -1,6 +1,7 @@
 package com.caparicio.springcloud.msvc.usuarios.models.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,14 +11,17 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Getter
 @Setter
 @Entity
-@Table(name="usuarios")
+@Table(name = "usuarios")
 public class Usuario {
   @Id
   @GeneratedValue(strategy = IDENTITY)
   private Long id;
   @NotEmpty
   private String nombre;
+  @NotEmpty
+  @Email
   @Column(unique = true)
   private String email;
+  @NotEmpty
   private String password;
 }
